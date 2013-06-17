f1f6cb9c2f4524e20b7b1923d00611607ee929cd
Matt Robenolt <matt@ydekproductions.com>
2013-06-17 11:07:24 -0700

    Add a WaitGroup and a progress bar

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"strings"
 	"time"
+	"github.com/cheggaaa/pb"
+	"sync"
 )
 
 const (
@@ -24,7 +26,7 @@ var (
 
 var value string
 
-func listen(channel chan string) {
+func listen(channel chan string, wg *sync.WaitGroup, bar *pb.ProgressBar) {
 	socket, err := net.Dial("tcp", *host)
 	if err != nil {
 		log.Fatal(err)
@@ -35,6 +37,8 @@ func listen(channel chan string) {
 	for {
 		key := <- channel
 		fmt.Fprintf(socket, SET, key, *size, value)
+		bar.Increment()
+		wg.Done()
 	}
 }
 
@@ -42,11 +46,13 @@ func main() {
 	flag.Parse()
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	i := 0
+	bar := pb.New(*iterations)
+	wg := new(sync.WaitGroup)
 	log.Printf("Making %d workers...", *workers)
 	channels := make([]chan string, *workers)
 	for i = 0; i < *workers; i++ {
 		channels[i] = make(chan string)
-		go listen(channels[i])
+		go listen(channels[i], wg, bar)
 	}
 	value = strings.Repeat("x", *size)
 	keys := make([]string, *iterations)
@@ -58,11 +64,15 @@ func main() {
 	}
 
 	log.Printf("Rock and roll.")
+	bar.Start()
 	start := time.Now()
 	for i = 0; i < *iterations; i++ {
+		wg.Add(1)
 		channels[i % *workers] <- keys[i]
 	}
+	wg.Wait()
 	duration := float32(time.Now().Sub(start).Nanoseconds())/1E9
+	bar.Finish()
 	log.Printf("Requests %f/s", float32(*iterations) / duration)
 	log.Printf("Duration %f", duration)
 }
